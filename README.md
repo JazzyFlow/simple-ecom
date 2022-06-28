7cd37576780aefecfe0b00e6296916b1aee88d63
YZ Cacho <yzcacho@YZs-MacBook-Pro.local>
2022-06-28 18:16:48 +0800

    added components

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 Ecommerce React App
+
+New/Edit product page
+Store page
+Show single product page
+Cart page
+A repository in github.
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import { CgMenuLeft } from 'react-icons/cg';
+
+function Navbar() {
+    return (
+        <div className='Navbar'>
+            <CgMenuLeft size={"2em"}/>
+        </div>
+    );
+}
+
+export default Navbar
